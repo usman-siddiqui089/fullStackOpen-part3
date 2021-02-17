53cc074c7904fc5b3abd0917407dbcbaabc8a089
usman-siddiqui089 <54310966+usman-siddiqui089@users.noreply.github.com>
2021-02-17 13:13:28 +0500

    Phonebook Backend Exercises (#1)
    * Added phonebook backend step1
    
    * Added phonebook backend step2
    
    * Added phonebook backend step3
    
    * Added phonebook backend step4
    
    * Added phonebook backend step5
    
    * Added phonebook backend step6
    
    * Added phonebook backend step7
    
    * Added phonebook backend step8
    
    * Added phonebook backend step9
    
    Co-authored-by: UsmanSiddiqui <usmansiddiqui@A006-427.local>

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+let persons = [
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: "12-42-234345"
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendick',
+        number: "39-23-6423122"
+    },
+]
+app.use(cors())
+app.use(express.json()) //json-parser
+morgan.token('body',(req,res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request,response) => {
+    const entriesCount = persons.length
+    const date = new Date()
+    response.send(`
+        <div>
+            <p>Phonebook has info for ${entriesCount} people</p>
+            <p>${date}</p>
+        </div>`)
+})
+
+app.get('/api/persons/:id', (request,response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person){
+        response.json(person)
+    }
+    else{
+        return response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+function getRandomID() {
+    return Math.floor(Math.random() * 100000);
+}
+
+app.post('/api/persons/', (request,response) => {
+    const body = request.body
+    const isDuplicate = persons.find(person => person.name === body.name)
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: 'Name or Number is missing. Please try again.'
+        })
+    }
+    else if(isDuplicate){
+        return response.status(400).json({
+            error: 'Duplicate name exists. Try new.'
+        })
+    }
+    else{
+        const newPerson = {
+            id: getRandomID(),
+            name: body.name,
+            number: body.number
+        }
+        persons = persons.concat(newPerson)
+        response.json(newPerson)
+    }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
