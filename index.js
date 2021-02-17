79b7093fa17229894ec28f897ee6f24af8e9dcc0
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-02-17 13:08:32 +0500

    Added phone backend step9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -24,6 +25,7 @@ let persons = [
         number: "39-23-6423122"
     },
 ]
+app.use(cors())
 app.use(express.json()) //json-parser
 morgan.token('body',(req,res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -87,7 +89,7 @@ app.post('/api/persons/', (request,response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`)
 })
