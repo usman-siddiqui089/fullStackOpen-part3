ddd3f5d8d7074166e8f45cc2ffaf82ec4649f107
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-02-20 21:26:47 +0500

    Added phonebook backend lint config

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const Person = require('./models/person')
 const app = express()
 
 let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: "12-42-234345"
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: "39-23-6423122"
-    },
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-42-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  },
 ]
 app.use(cors())
 app.use(express.static('build'))
@@ -34,110 +34,110 @@ morgan.token('body',(req,res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request,response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request,response,next) => {
-    Person.find({})
-        .then(result => {
-            const entriesCount = result.length
-            const date = new Date()
-            response.send(`
+  Person.find({})
+    .then(result => {
+      const entriesCount = result.length
+      const date = new Date()
+      response.send(`
                 <div>
                     <p>Phonebook has info for ${entriesCount} people</p>
                     <p>${date}</p>
                 </div>
             `)
-        })
-        .catch(error => {
-            next(error)
-        })
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.get('/api/persons/:id', (request,response,next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            response.json(person)
-        })
-        .catch(error => {
-            next(error)
-        })
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            console.log(result)
-            response.status(204).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-    const newPerson = {
-        name: body.name,
-        number: body.number
-    }
-    Person.findByIdAndUpdate(request.params.id, newPerson, {new: true})
-        .then(updatedPerson => {
-            console.log('UPDATED PERSON: ',updatedPerson)
-            response.json(updatedPerson)
-        })
-        .catch(error => {
-            next(error)
-        })
+  const body = request.body
+  const newPerson = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, newPerson, { new: true })
+    .then(updatedPerson => {
+      console.log('UPDATED PERSON: ',updatedPerson)
+      response.json(updatedPerson)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.post('/api/persons/', (request,response, next) => {
-    const body = request.body
-    const isDuplicate = persons.find(person => person.name === body.name)
-    if(!body.name || !body.number){
-        return response.status(400).json({
-            error: 'Name or Number is missing. Please try again.'
-        })
-    }
-    else if(isDuplicate){
-        return response.status(400).json({
-            error: 'Duplicate name exists. Try new.'
-        })
-    }
-    else{
-        const newPerson = new Person({
-            name: body.name,
-            number: body.number
-        })
-        newPerson.save()
-            .then(savedPerson => savedPerson.toJSON())
-            .then(savedAndFormattedPerson => {
-                response.json(savedAndFormattedPerson)
-            })
-            .catch(error => {
-                next(error)
-            })
-    }
+  const body = request.body
+  const isDuplicate = persons.find(person => person.name === body.name)
+  if(!body.name || !body.number){
+    return response.status(400).json({
+      error: 'Name or Number is missing. Please try again.'
+    })
+  }
+  else if(isDuplicate){
+    return response.status(400).json({
+      error: 'Duplicate name exists. Try new.'
+    })
+  }
+  else{
+    const newPerson = new Person({
+      name: body.name,
+      number: body.number
+    })
+    newPerson.save()
+      .then(savedPerson => savedPerson.toJSON())
+      .then(savedAndFormattedPerson => {
+        response.json(savedAndFormattedPerson)
+      })
+      .catch(error => {
+        next(error)
+      })
+  }
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if(error.name === 'CastError'){
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if(error.name === 'ValidationError'){
-        return response.status(400).send(error.message)
-    }
-    next(error)
+  console.error(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).send(error.message)
+  }
+  next(error)
 }
 app.use(errorHandler)
 const PORT = process.env.PORT
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 if(process.argv.length < 3 || process.argv.length > 5){
-    console.log('Please provide the password as an argument: node mongo.js <password> <optional args>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password> <optional args>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,30 +11,30 @@ const url = `mongodb+srv://fullstackopen:${password}@cluster0.2dcnc.mongodb.net/
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 const Person = mongoose.model('Person',personSchema)
 if(process.argv.length === 3){
-    Person.find({}).then(result => {
-        console.log('Phonebook')
-        result.forEach(record => {
-            console.log(`${record.name} ${record.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('Phonebook')
+    result.forEach(record => {
+      console.log(`${record.name} ${record.number}`)
     })
+    mongoose.connection.close()
+  })
 }
 else if(process.argv.length === 5){
-    const person = new Person({
-        name,
-        number
-    })
-    person.save().then(result => {
-        console.log(`added ${name} number ${number} to Phonebook.`)
-        mongoose.connection.close()
-    })
+  const person = new Person({
+    name,
+    number
+  })
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to Phonebook.`)
+    mongoose.connection.close()
+  })
 }
 else {
-    console.log('Please provide correct arguments and try again!')
-    process.exit(1)
+  console.log('Please provide correct arguments and try again!')
+  process.exit(1)
 }
