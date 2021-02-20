7d6836872f258f608cb080307a033ea6a436cb30
UsmanSiddiqui <usmansiddiqui@A006-427.local>
2021-02-20 18:01:33 +0500

    Added phonebook database step3

--- index.js
+++ index.js
@@ -56,9 +56,15 @@ app.get('/api/persons/:id', (request,response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            console.log(result)
+            response.status(204).end()
+        })
+        .catch(err => {
+            console.error(err)
+            response.status(404).end()
+        })
 })
 
 app.post('/api/persons/', (request,response) => {
